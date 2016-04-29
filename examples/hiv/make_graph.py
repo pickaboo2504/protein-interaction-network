f5dc120b161813b486fe866c14f447819c5f6acb
Eric Ma <ericmajinglong@gmail.com>
2016-04-29 06:15:41 -0400

    added examples using protein graphs

---
+++ examples/hiv/make_graph.py
@@ -0,0 +1,9 @@
+from pin import pin
+import networkx as nx
+
+"""
+Makes a graph out of a PDB file and saves the graph as a NetworkX pickle.
+"""
+
+p = pin.ProteinInteractionNetwork('hiv1_homology_model.pdb')
+nx.write_gpickle(p, 'hiv1_homology_model.pkl')
