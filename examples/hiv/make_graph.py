24b641a4321eebc997e7e654f8467c9f4e303238
pickaboo2504 <117581268+pickaboo2504@users.noreply.github.com>
2024-07-12 12:21:36 +0600

    Delete examples directory

--- examples/hiv/make_graph.py
+++
@@ -1,8 +0,0 @@
-"""Make a graph out of a PDB file and saves the graph as a NetworkX pickle."""
-
-import networkx as nx
-
-from proteingraph import read_pdb
-
-p = read_pdb("hiv1_homology_model.pdb")
-nx.write_gpickle(p, "hiv1_homology_model.pkl")
